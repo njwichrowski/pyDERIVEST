bb7c86470b156b9772e8362ecc155dbff714c768
Noah J. Wichrowski <53314923+njwichrowski@users.noreply.github.com>
2019-07-29 14:07:46 -0400

    Create __init__.py

---
+++ derivest/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ["derivest", "directional_diff", "gradest", "hess_diag", "hessian", "jacobianest", "ensemble"]
+
+from .derivest import derivest
+from .directional_diff import directional_diff
+from .gradest import gradest
+from .hess_diag import hess_diag
+from .hessian import hessian
+from .jacobianest import jacobianest
+from .ensemble import ensemble
